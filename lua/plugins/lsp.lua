63776da3ced5384e6d85c7f3f1fcb051d18b2000
Reqox <anovikova999666@gmail.com>
2026-01-16 17:13:13 +0300

    blabla

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,10 +9,6 @@ return {
 
 	config = function()
 		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = { "ts_ls", "lua_ls", "html", "cssls", "jsonls", "sqlls", "emmet_ls" },
-			automatic_installation = true,
-		})
 
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 		local capabilities = cmp_nvim_lsp.default_capabilities()
@@ -29,23 +25,16 @@ return {
 		local on_attach = function(client, bufnr)
 			local opts = { buffer = bufnr, silent = true }
 
-			-- Навигация
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-
-			-- Документация и подсказки
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-
-			-- Рефакторинг
 			vim.keymap.set("n", "grn", vim.lsp.buf.rename, opts)
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-
-			-- Диагностика
 			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 			vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 		end
 
-		-- Настройка для ts_ls и emmet_ls
+		-- Кастомизация конфигов через vim.lsp.config()
 		vim.lsp.config("ts_ls", {
 			capabilities = capabilities,
 			on_attach = on_attach,
@@ -57,15 +46,6 @@ return {
 			on_attach = on_attach,
 			root_dir = find_root_dir,
 		})
-
-		-- Настройка для остальных серверов
-		local other_servers = { "html", "cssls", "jsonls", "sqlls" }
-		for _, server in ipairs(other_servers) do
-			vim.lsp.config(server, {
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
-		end
 
 		vim.lsp.config("lua_ls", {
 			capabilities = capabilities,
@@ -83,49 +63,24 @@ return {
 			},
 		})
 
-		-- Автозапуск серверов при открытии файлов
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-			callback = function()
-				vim.lsp.enable("ts_ls")
-				vim.lsp.enable("emmet_ls")
-			end,
-		})
+		-- Для остальных серверов просто capabilities и on_attach
+		for _, server in ipairs({ "html", "cssls", "jsonls", "sqlls" }) do
+			vim.lsp.config(server, {
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+		end
 
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "html" },
-			callback = function()
-				vim.lsp.enable("html")
-				vim.lsp.enable("emmet_ls")
-			end,
-		})
-
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "css", "scss" },
-			callback = function()
-				vim.lsp.enable("cssls")
-			end,
-		})
-
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "json" },
-			callback = function()
-				vim.lsp.enable("jsonls")
-			end,
-		})
-
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "sql" },
-			callback = function()
-				vim.lsp.enable("sqlls")
-			end,
-		})
-
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "lua" },
-			callback = function()
-				vim.lsp.enable("lua_ls")
-			end,
+		-- Mason-lspconfig с handlers для автозапуска
+		require("mason-lspconfig").setup({
+			ensure_installed = { "ts_ls", "lua_ls", "html", "cssls", "jsonls", "sqlls", "emmet_ls" },
+			automatic_installation = true,
+			handlers = {
+				-- Дефолтный handler для всех серверов
+				function(server_name)
+					vim.lsp.enable(server_name)
+				end,
+			},
 		})
 	end,
 }
