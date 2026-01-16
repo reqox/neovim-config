7373f47f59d945ea7f57686fc57d2240b4fa42b5
Reqox <anovikova999666@gmail.com>
2026-01-16 16:23:11 +0300

    blabla

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,27 +1,45 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  event = { 'BufReadPost', 'BufNewFile' },
-  opts = {
-    -- Включаем подсветку синтаксиса
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
+	"nvim-treesitter/nvim-treesitter",
+	version = "v0.9.3",
+	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
+	opts = {
+		-- Включаем подсветку синтаксиса
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
 
-    -- Умное выделение текста (индентация)
-    indent = { enable = true },
+		-- Умное выделение текста (индентация)
+		indent = { enable = true },
 
-    -- Список парсеров
-    ensure_installed = {
-      -- Frontend & Backend
-      'javascript', 'typescript', 'tsx', 'jsx', 'vue', 'html', 'css', 'scss', 'sql',
-      -- Other
-      'lua', 'vim', 'vimdoc', 'json', 'yaml', 'markdown', 'markdown_inline', 'bash', 'dockerfile', 'gitignore',
+		-- Список парсеров
+		ensure_installed = {
+			-- Frontend & Backend
+			"javascript",
+			"typescript",
+			"tsx",
+			"jsx",
+			"vue",
+			"html",
+			"css",
+			"scss",
+			"sql",
+			-- Other
+			"lua",
+			"vim",
+			"vimdoc",
+			"json",
+			"yaml",
+			"markdown",
+			"markdown_inline",
+			"bash",
+			"dockerfile",
+			"gitignore",
 
-      'python',
-      'c',
-      'cpp',
-    },
-  },
+			"python",
+			"c",
+			"cpp",
+		},
+	},
 }
