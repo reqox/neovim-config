ff8e5300700f7039894df6a0cbecc4013c5887db
Reqox <anovikova999666@gmail.com>
2026-01-20 15:11:32 +0300

    Install neovim-tree.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,10 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- NETWR SETTINGS
+-- vim.g.netrw_banner = false
 vim.g.netrw_liststyle = 3 -- Древовидный Explorer (:Ex). 3 раза i
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 2
@@ -9,7 +12,7 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.wrap = true
-vim.opt.scrolloff = 4
+vim.opt.scrolloff = 2
 vim.opt.termguicolors = true
 vim.opt.mouse = ""
 
